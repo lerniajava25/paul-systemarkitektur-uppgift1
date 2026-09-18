d1dbe07ecba14e4437dc28ef80cc8bbe616d31d6
Paul Ibrahim <paul.ibrahim@hotmail.se>
2026-09-18 16:29:40 +0200

    implement builder pattern for product

---
+++ src/main/java/Product.java
@@ -0,0 +1,105 @@
+import java.text.Format;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class Product {
+
+    private final String id;
+    private final String name;
+    private final Category category;
+    private final int rating;
+    private final LocalDateTime createdAt;
+    private final LocalDateTime updatedAt;
+
+    private Product(Builder builder) {
+        this.id = builder.id;
+        this.name = builder.name;
+        this.category = builder.category;
+        this.rating = builder.rating;
+        this.createdAt = builder.createdAt;
+        this.updatedAt = builder.updatedAt;
+    }
+
+    public static class Builder {
+
+        private String id;
+        private String name;
+        private Category category;
+        private int rating;
+        private LocalDateTime createdAt;
+        private LocalDateTime updatedAt;
+
+
+        public Builder id(String id) {
+            this.id = id;
+            return this;
+        }
+
+        public Builder name(String name) {
+            this.name = name;
+            return this;
+        }
+
+        public Builder category(Category category) {
+            this.category = category;
+            return this;
+        }
+
+        public Builder rating(int rating) {
+            this.rating = rating;
+            return this;
+        }
+
+
+        public Builder createdAt(LocalDateTime createdAt) {
+            this.createdAt = createdAt;
+            return this;
+        }
+
+        public Builder updatedAt(LocalDateTime updatedAt) {
+            this.updatedAt = updatedAt;
+            return this;
+        }
+
+        public Product build() {
+            if (name == null || name.isEmpty()) {
+                throw new IllegalArgumentException("Name cannot be empty");
+            }
+
+            if (createdAt == null) {
+                createdAt = LocalDateTime.now();
+            }
+
+            if (updatedAt == null) {
+                updatedAt = LocalDateTime.now();
+            }
+            return new Product(this);
+        }
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+
+}
